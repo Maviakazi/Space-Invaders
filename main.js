833f2b06e90f7e9da9c5eb40f1e368fcf0e78dd8
Mavia Kazi <mavia.salman.kazi@gmail.com>
2023-10-22 13:44:22 +1100

    Move shooter

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
 /* Constants */
 
 /* Declare variables */
-
+let alienInvaders = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    25, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+];
+let shooterIndex = 217;
 /* Cached HTML elements */
 const boardEl = document.querySelector('.board');
 
+// Create square divs on the board
 for (let i = 0; i < 225; i++) {
     let squareEl = document.createElement('div');
     boardEl.appendChild(squareEl);
 }
+
+// Capturing all the squares in a variable then turn it in to an array
 const squaresEl = document.querySelectorAll('.board div');
 let squaresArray = Array.from(squaresEl);
-console.log(squaresArray);
+// console.log(squaresArray);
 
-init();
+// Functions
 
-function init() {
-    let alienInvaders = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 17, 18, 19, 20];
+// Draw shooter and alien Invaders on
+function draw() {
+    for (let i = 0; i < alienInvaders.length; i++) {
+        squaresArray[alienInvaders[i]].classList.add('invader');
+    }
+    squaresArray[shooterIndex].classList.add('shooter');
 }
 
-function drawInvaders() {}
+draw();
+
+function moveShooter(event) {
+    squaresArray[shooterIndex].classList.remove('shooter');
+    switch (event.key) {
+        case 'ArrowLeft':
+            if (shooterIndex >= 211) shooterIndex -= 1;
+            break;
+
+        case 'ArrowRight':
+            if (shooterIndex !== 224) shooterIndex += 1;
+            break;
+    }
+    squaresArray[shooterIndex].classList.add('shooter');
+}
+
+// Event Listeners
+
+window.addEventListener('keydown', moveShooter);
